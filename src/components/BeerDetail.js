cbbe467c0201c28757e780246175859368e5db94
Quentin Crida <qcrida@gmail.com>
2020-05-13 08:05:39 +0100

    more details added to Detail

--- src/components/BeerDetail.js
+++ src/components/BeerDetail.js
@@ -3,7 +3,12 @@ import React from 'react';
 const BeerDetail = (props) => {
   if(!props.beer) return null;
   return (
-    <h3>{props.beer.name}</h3>
+    <div>
+      <h2>Name: {props.beer.name}</h2>
+      <h3>What am I like? "{props.beer.tagline}"</h3>
+      <p>Tasting notes: "{props.beer.description}"</p>
+      <img src="https://images.punkapi.com/v2/keg.png" alt="keg image"/>
+    </div>
   )
 
 }
